7fb4735bafa4649b29ff8326e53c91964eacb579
AoutikArya <90469048+AoutikArya@users.noreply.github.com>
2022-07-09 12:31:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import streamlit as st
 import pandas as pd
 import joblib
 from PIL import Image
 image = Image.open('house.png')
-
-# Title
 st.image(image, width=600)
-
-# Title
 st.header("Boston House Price Prediction")
-
-# Input bar 1
 crim = st.number_input("Enter CRIM")
 zn = st.number_input("Enter ZN")
 indus = st.number_input("Enter INDUS")
-
 nox = st.number_input("Enter NOX")
 rm = st.number_input("Enter RM")
 age = st.number_input("Enter AGE")
@@ -29,25 +16,18 @@ rad = st.number_input("Enter RAD")
 tax = st.number_input("Enter TAX")
 ptratio = st.number_input("Enter PTRATIO")
 b = st.number_input("Enter B")
-
 lstat = st.number_input("Enter Lstat")
-
 chas = st.selectbox("Select CHAS", (0, 1))
 
 if st.button("Submit"):
     
     # Unpickle classifier
     lr = joblib.load("lr.pkl")
-    
-
-    X=pd.DataFrame([[crim,zn,indus,nox,rm,age,dis,rad,tax,ptratio,b,lstat,chas]],columns=['CRIM', 'ZN', 'INDUS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX',
+    X=pd.DataFrame([[crim,zn,indus,nox,rm,age,dis,rad,tax,ptratio,b,lstat,chas]],
+                   columns=['CRIM', 'ZN', 'INDUS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX',
            'PTRATIO', 'B', 'LSTAT','CHAS'])
-
     prediction = lr.predict(X)[0]               
-    st.text(f"The house price is  {prediction}")               
-
-
-# In[ ]:
+    st.text(f"The house price is  {prediction}")     
 
 
 
